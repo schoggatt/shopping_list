723f05b0e51b900d6a3cecfd7c0597be16044985
Sam Hoggatt <schoggatt@users.campus>
2020-02-24 17:01:20 -0600

    done

---
+++ ShoppingList/AddItemControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for AddItemControl.xaml
+    /// </summary>
+    public partial class AddItemControl : UserControl
+    {
+        public AddItemControl()
+        {
+            InitializeComponent();
+            AddButton.Click += OnAddItemButton_Click;
+        }
+
+        protected void OnAddItemButton_Click(object sender, RoutedEventArgs args)
+        {
+            var item = ItemToAdd.Text;
+            if(DataContext is ShoppingListData data) data.Add(item);
+            ItemToAdd.Text = "";
+        }
+    }
+}
